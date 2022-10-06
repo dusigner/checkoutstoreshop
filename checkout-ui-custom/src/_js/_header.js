30be9c7e354acd845527fd816a5bd1790d2d284e
Daniel Miranda <daniel.sousa@trinto.com.br>
2022-10-06 09:55:02 -0300

    segregate custom header and fix steps problem

---
+++ checkout-ui-custom/src/_js/_header.js
@@ -0,0 +1,59 @@
+export default class CustomHeader {
+  init() {
+    this._conditionalHeader()
+    this._backToCartLink()
+  }
+
+  _conditionalHeader() {
+    try {
+      steps(window.location.hash)
+      /* eslint-disable-next-line no-restricted-globals */
+      addEventListener('hashchange', event => {
+        const { hash } = event.target.location
+
+        steps(hash)
+      })
+    } catch (e) {
+      console.error('conditionalHeader error', e)
+    }
+
+    function steps(hash) {
+      const _stepElem = $(`.checkout-steps`)
+      const _gotoCartElem = $(`#go-to-cart-button-custom`)
+      const _headerElem = $(`.main-header`)
+
+      const showHeader = ['#/payment', '#/shipping', '#/profile']
+
+      if (showHeader.includes(hash)) {
+        _stepElem.css('display', 'block')
+        _gotoCartElem.css('display', 'flex')
+        _headerElem.css('box-shadow', '0px 4px 10px #00000033')
+      } else {
+        _stepElem.css('display', 'none')
+        _gotoCartElem.css('display', 'none')
+        _headerElem.css('box-shadow', 'none')
+      }
+    }
+  }
+
+  _backToCartLink() {
+    try {
+      const _stepElem = $(`.checkout-steps`)
+      const _containerElem = $(`.main-header .container`)
+
+      if (_containerElem.find('#go-to-cart-button-custom').length > 0) {
+        return
+      }
+
+      _stepElem.before(`
+        <p id="go-to-cart-button-custom" class="link link-cart pull-right" data-bind="if: !window.router.sac.isActive()">
+          <small>
+            <a data-bind="attr: { href: window.checkout.cartURL() }" id="orderform-minicart-to-cart" target="_self" data-event="orderformToCart" data-i18n="global.backToCart" href="#/cart">Voltar para o carrinho</a>
+          </small>
+        </p>
+      `)
+    } catch (e) {
+      console.error('backToCart error', e)
+    }
+  }
+}
