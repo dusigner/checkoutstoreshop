14e755c1eae0b9620efda438fc98252b72672b19
David Torres <david.torres@partner.sdslasupport.com>
2022-10-21 09:24:53 -0300

    change coupon placeholder

--- checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
+++ checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
@@ -1342,6 +1342,16 @@ class checkoutCustom {
         setTimeout(() => $('#cart-shipping-calculate').click(), 10)
       }
     })
+
+    $('body').on('input', '#cart-coupon', function () {
+      const $this = $(this)
+
+      if ($this.val()) {
+        $this.closest('span').addClass('has-value')
+      } else {
+        $this.closest('span').removeClass('has-value')
+      }
+    })
   }
 
   init() {
