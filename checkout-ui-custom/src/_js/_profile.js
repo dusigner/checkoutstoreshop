fa3a59dfbe73e918afdd78a0cd5bc2d2d74e7704
lucas.poio <lucas.poio@samsung.com>
2023-01-05 15:07:06 -0300

    feature/ecoms-3084 Adjusts texts and checkbox opt in checkout profile, only one opt in

--- checkout-ui-custom/src/_js/_profile.js
+++ checkout-ui-custom/src/_js/_profile.js
@@ -185,25 +185,26 @@ export default class CustomProfileData {
 
     const $field = `<div class="newsletter-optin">
       <h3>Newsletter e Promoções (opcional)</h3>
-      <label class="inputOptIn __whatsapp">
-          <input type="checkbox" id="inputWhatsapp" />
-          <span class="custom-checkbox-icon"></span>
-          <span>Desejo receber ofertas e notificações por WhatsApp.</span>
-          <span class="form-tooltip">
-              <img alt="info" class="info-img form-tooltip__initiator"
-                  src="https://samsungbrtest.vteximg.com.br/arquivos/info.png" />
-              <span class="form-tooltip__item">Você receberá atualizações do seu pedido e mensagens sobre ofertas.</span>
-          </span>
-      </label>
+      <label class="inputOptIn __newsletter" />
     </div>`
 
     $('#client-profile-data p.save-data').after($field)
 
     // moves emails and offers into this context
     $('.newsletter-text').before('<span class="custom-checkbox-icon"></span>')
+    // $('#newsletter-text-span').show()
+    // $('.newsletter-text').hide()
+    $('.newsletter-label').append('<span class="newsletter-text-correct">Desejo receber comunicações, ofertas e novidades sobre a Samsung.</span>');
+    // Troca de texto do newsletter
+    // $(".newsletter-label").html('<p class="text-success">Teste</p>');
+    // $('.checkbox newsletter-label').append('<span class="newsletter-text" data-i18n="global.optinNewsLetter">Desejo receber comunicações, ofertas e novidades sobre a Samsung.</span>');
+    // const element = $(".newsletter-text");
+    // const textToReplace = element.text();
+    // const newText = textToReplace.replace("Quero receber e-mails com promoções.", "Desejo receber comunicações, ofertas e novidades sobre a Samsung.");
+    // element.text(newText); 
     const $infoEmail = $('.box-client-info .newsletter').detach()
+    $('.box-client-info .__newsletter').after($($infoEmail))
 
-    $('.box-client-info .__whatsapp').after($($infoEmail))
   }
 
   addTermsAndPolicies() {
