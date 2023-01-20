d0b3a65643ea2737079adf4e901feebfa35a157f
fnicomedes <fnicomedes@gmail.com>
2023-01-20 12:18:50 -0300

    fix: btn login samsung

--- checkout-ui-custom/src/_js/_pre-email.js
+++ checkout-ui-custom/src/_js/_pre-email.js
@@ -10,6 +10,20 @@ export default class CustomPreEmail {
       $('.samsung-account-container').length === 0 &&
       $('.client-pre-email-h').length > 0
     ) {
+
+      $("#toggleButtonContainer").remove()
+
+      $("#btn-client-pre-email").after(`
+      <div id="toggleButtonContainer" class="samsung-toggle-button-container">
+
+        <p class="samsung-visitante-title">Entrar como visitante:</p>
+
+        <button class="samsung-button-login" id="toggleButtonLogin" type="button">informe seu e-mail</button>
+
+        <p class="samsung-message-rewards">Ao entrar como visitante não será possível pontuar no programa Samsung Rewards</p>
+      </div>
+    `)
+
       $('.client-pre-email-h').after(`
           <div class="samsung-account-container">
             <img
@@ -17,6 +31,11 @@ export default class CustomPreEmail {
               src="https://samsungbr.vteximg.com.br/arquivos/logo-ssg-account-black.svg"
             />
             <div class="samsung-account-image">
+            <div class="samsung-account-label" style="font-size:12px; font-family:'SamsungOne'; margin-top: 20px; justify-content: center; display: flex;">
+            <p style="max-width: 326px">
+              Suas compras podem valer pontos para utilização como desconto na loja Samsung. Participe agora para garantir sua pontuação.
+            </p>
+          </div>
               <button
                 id="btn-samsung-account"
                 type="submit"
@@ -28,12 +47,7 @@ export default class CustomPreEmail {
               <img style="padding-right: 11px; border-right: 1px solid #cbcbcb" src="https://samsungbr.vteximg.com.br/arquivos/logo-rewards.png?v=1" />
               <img style="margin-left: 10px" src="https://samsungbr.vteximg.com.br/arquivos/logo-frete.png?v=1" />
             </div>
-            <div class="samsung-account-label" style="font-size:12px; font-family:'SamsungOne'; margin-top: 20px; justify-content: center; display: flex;">
-              <p style="max-width: 326px">
-                Suas compras podem valer pontos para utilização como desconto na loja Samsung. Participe agora para garantir sua pontuação.
-              </p>
-            </div>
-            <div class="samsung-account-create" style="font-size:12px; font-family:'SamsungOne'; justify-content: center; display: flex;">
+            <div class="samsung-account-create" style="margin-top: 10px; font-size:12px; font-family:'SamsungOne'; justify-content: center; display: flex;">
               <p style="max-width: 326px">
                 Não tem uma Samsung Account?
                 <a href="https://account.samsung.com/membership" target="_blank" style="color: black; text-decoration: underline">Registre-se agora</a>.
@@ -72,6 +86,10 @@ export default class CustomPreEmail {
 
     $(document).on('click', '#btn-samsung-account', function () {
       _this.openSamsungAccountModal()
+    })
+
+    $(document).on('click', '#toggleButtonLogin', function () {
+      $('.samsung-toggle-button-container').hide();
     })
   }
 }
