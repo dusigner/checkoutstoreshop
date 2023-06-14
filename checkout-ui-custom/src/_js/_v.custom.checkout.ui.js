8d6462f6f8a17bd465457bdbae80fc58c5549947
Deyvison Silva <deyvison.silva@partner.sdslasupport.com>
2023-06-14 14:04:39 -0300

    feat(tradein): adjust gti discount description

--- checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
+++ checkout-ui-custom/src/_js/_v.custom.checkout.ui.js
@@ -423,7 +423,7 @@ class checkoutCustom {
         if (discount.name.toLowerCase().includes('garanteed')) {
           return `
             <tr class="discount garanteed-tradein" style="height: 23px;">
-              <td style="margin-left: 10px;">Avaliação Turbinada - Troca Smart</td>
+              <td style="margin-left: 10px;">Vale Mais - Troca Smart</td>
               <td>
                 <span style="font-weight: 700" >${formatNegativeValue(
                   formatCurrencyBRL(discount.value)
