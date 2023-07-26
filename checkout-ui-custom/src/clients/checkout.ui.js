9e0f41a177a6e3ef26fe06b0858cd1358e58b273
Paulo Bordignon <paulo.bordignon@partner.sdslasupport.com>
2023-07-26 16:46:50 -0300

    feat: add logo to fidelity partner

--- checkout-ui-custom/src/clients/checkout.ui.js
+++ checkout-ui-custom/src/clients/checkout.ui.js
@@ -11,6 +11,7 @@ import CheckoutLimit from '../components/_checkoutLimit'
 import SamsungCarePlus from '../components/_samsungCarePlus'
 import Messages from '../components/_messages'
 import ShippingEstimateCustom from '../components/_shippingEstimateCustom'
+import FidelidadeCustomizations from '../components/_fidelidade'
 import { rootPath } from '../components/utils/_rootPath'
 
 import {
@@ -54,6 +55,7 @@ export class CheckoutCustom {
     this.CheckoutLimit = new CheckoutLimit()
     this.samsungCarePlus = new SamsungCarePlus()
     this.messages = new Messages()
+    this.fidelidade = new FidelidadeCustomizations()
 
     if (deliveryDateFormat) {
       this.shippingEstimateCustom = new ShippingEstimateCustom()
@@ -282,19 +284,18 @@ export class CheckoutCustom {
         const isSamsungCare = detailUrl.includes('/samsung-care-/p')
 
         const shippingText =
-          isInstallService || isSamsungCare
-            ? 'Após a entrega do produto'
-            : ''
+          isInstallService || isSamsungCare ? 'Após a entrega do produto' : ''
 
         const moreInfoHtml = `
             <div class="more-info">
               <p class="ref-id" style="font-size: 12px" data-refid="${refId}">${refId}</p>
               <p class="estimate-shipping">${shippingText}</p>
               <p class="instantvoucher">
-                <a class="selecaovoucher" href='${detailUrl.split('/p')[0] +
-          '/instant-voucher?skuId=' +
-          orderForm.items[i].id
-          }'>Voltar à seleção de cupom instantâneo</a>
+                <a class="selecaovoucher" href='${
+                  detailUrl.split('/p')[0] +
+                  '/instant-voucher?skuId=' +
+                  orderForm.items[i].id
+                }'>Voltar à seleção de cupom instantâneo</a>
               </p>
             </div>
           `
@@ -394,16 +395,16 @@ export class CheckoutCustom {
         const name = discount.ratesAndBenefitsIdentifier
           ? discount.ratesAndBenefitsIdentifier.name
           : discount.name
-            ? discount.name
-            : ''
+          ? discount.name
+          : ''
 
         const total = itemsDiscounts.reduce(function (acc, current) {
           const isDiscountInCash = current.ratesAndBenefitsIdentifier
             ? current.ratesAndBenefitsIdentifier.name
 
-              .toLowerCase()
-              .includes('desconto à vista') &&
-            name.toLowerCase().includes('desconto à vista')
+                .toLowerCase()
+                .includes('desconto à vista') &&
+              name.toLowerCase().includes('desconto à vista')
             : ''
 
           if (current.name === discount.name || isDiscountInCash) {
@@ -437,8 +438,8 @@ export class CheckoutCustom {
               <td style="margin-left: 10px;">Desconto ${paymentSystemName}</td>
               <td>
                 <span style="font-weight: 700">${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                  formatCurrencyBRL(discount.value)
+                )}</span>
               </td>
             </tr>`
         }
@@ -449,8 +450,8 @@ export class CheckoutCustom {
                 <td style="margin-left: 10px;">Desc. Cupom Instantâneo</td>
                 <td>
                   <span style="font-weight: 700" >${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                    formatCurrencyBRL(discount.value)
+                  )}</span>
                 </td>
               </tr>`
         }
@@ -461,19 +462,24 @@ export class CheckoutCustom {
                 <td style="margin-left: 10px;">Desc. Samsung Care+</td>
                 <td>
                   <span style="font-weight: 700" >${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                    formatCurrencyBRL(discount.value)
+                  )}</span>
                 </td>
               </tr>`
         }
-        if (discount.name.toLowerCase().includes('discount@manualprice') && window.vtexjs.checkout.orderForm.customData.customApps.some(app => app.id == 'eco_troca')) {
+        if (
+          discount.name.toLowerCase().includes('discount@manualprice') &&
+          window.vtexjs.checkout.orderForm.customData.customApps.some(
+            app => app.id == 'eco_troca'
+          )
+        ) {
           return `
               <tr class="discount eco_troca" style="height: 23px;">
                 <td style="margin-left: 10px;">Desc. Eco Troca</td>
                 <td>
                   <span style="font-weight: 700" >${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                    formatCurrencyBRL(discount.value)
+                  )}</span>
                 </td>
               </tr>`
         }
@@ -483,8 +489,8 @@ export class CheckoutCustom {
               <td style="margin-left: 10px;">Vale Mais - Troca Smart</td>
               <td>
                 <span style="font-weight: 700" >${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                  formatCurrencyBRL(discount.value)
+                )}</span>
               </td>
             </tr>`
         }
@@ -495,8 +501,8 @@ export class CheckoutCustom {
               <td style="margin-left: 10px;">Vale Mais - Troca Smart</td>
               <td>
                 <span style="font-weight: 700" >${formatNegativeValue(
-            formatCurrencyBRL(discount.value)
-          )}</span>
+                  formatCurrencyBRL(discount.value)
+                )}</span>
               </td>
             </tr>`
         }
@@ -513,8 +519,8 @@ export class CheckoutCustom {
               <td style="margin-left: 10px;">Desc. Cupom</td>
               <td>
                 <span style="font-weight: 700" >${formatNegativeValue(
-          formatCurrencyBRL(discount.value)
-        )}</span>
+                  formatCurrencyBRL(discount.value)
+                )}</span>
               </td>
             </tr>`
       })
@@ -600,16 +606,16 @@ export class CheckoutCustom {
         accumulator +
         (item.priceTags.length
           ? item.priceTags.filter(_pricetag => {
-            return _pricetag.ratesAndBenefitsIdentifier
-              ? _pricetag.ratesAndBenefitsIdentifier.matchedParameters[
-              'couponCode@Marketing'
-              ] === _coupon
-              : 0
-          }).length
+              return _pricetag.ratesAndBenefitsIdentifier
+                ? _pricetag.ratesAndBenefitsIdentifier.matchedParameters[
+                    'couponCode@Marketing'
+                  ] === _coupon
+                : 0
+            }).length
           : 0)
       )
     },
-      0)
+    0)
 
     if (!_coupon || couponItemsCount > 0) {
       $('.coupon-applied-message').remove()
@@ -856,13 +862,15 @@ export class CheckoutCustom {
     const tooltip = `
       <div class="vcustom-customTax-resume">
        ${customtax
-        .map(
-          i =>
-            `<p class="vcustom-customTax-resume__i"><span class="n">${i.name
-            }</span><span class="v">${orderForm.storePreferencesData.currencySymbol
-            } ${(i.value / 100).toFixed(2)}</span></p>`
-        )
-        .join('')}
+         .map(
+           i =>
+             `<p class="vcustom-customTax-resume__i"><span class="n">${
+               i.name
+             }</span><span class="v">${
+               orderForm.storePreferencesData.currencySymbol
+             } ${(i.value / 100).toFixed(2)}</span></p>`
+         )
+         .join('')}
       </div>
     `
 
@@ -898,8 +906,9 @@ export class CheckoutCustom {
 
       const _summaryOrder = `
         <div class="summaryOrder">
-          <h6>Resumo do pedido (${itemsQuantity} ${quantitySelectedItems.length <= 1 ? 'item' : 'itens'
-        })</h6>
+          <h6>Resumo do pedido (${itemsQuantity} ${
+        quantitySelectedItems.length <= 1 ? 'item' : 'itens'
+      })</h6>
           <ul>
             ${listItems}
           </ul>
@@ -951,7 +960,8 @@ export class CheckoutCustom {
           .prepend(
             `
           <div class="v-custom-quantity-price vqc-ldelem">
-            <p class="v-custom-quantity-price__best" style="font-size: 18px; color: #000; margin-bottom: 4px">${free ? 'Grátis' : totalValue
+            <p class="v-custom-quantity-price__best" style="font-size: 18px; color: #000; margin-bottom: 4px">${
+              free ? 'Grátis' : totalValue
             }</p>
           </div>
           `
@@ -976,7 +986,7 @@ export class CheckoutCustom {
           g => g.provider === 'SSG_REWARDS'
         )
 
-        let discount = 0;
+        let discount = 0
 
         if (
           giftRewards.length &&
@@ -992,8 +1002,8 @@ export class CheckoutCustom {
             <div class="best-price" style="font-size: 26px; display: flex; justify-content: space-between; font-weight: 700">
               <p class="ref-id">Total</p>
               <p class="estimate-shipping">${formatCurrencyBRL(
-            paymentAmountTotal - discount
-          )}</p>
+                paymentAmountTotal - discount
+              )}</p>
             </div>
           </div>
         `
@@ -1029,7 +1039,8 @@ export class CheckoutCustom {
         if (_this.lastOrderFormTotalPrice !== orderForm.value) {
           _this.lastOrderFormTotalPrice = orderForm.value
           _this.termPrice = await fetch(
-            `${rootPath()}/api/checkout/pub/orderForm/${orderForm.orderFormId
+            `${rootPath()}/api/checkout/pub/orderForm/${
+              orderForm.orderFormId
             }/installments?paymentSystem=2`
           )
             .then(response => response.json())
@@ -1060,15 +1071,16 @@ export class CheckoutCustom {
                 <div class="best-price" style="font-size: 26px; display: flex; justify-content: space-between; font-weight: 700">
                   <p class="ref-id">Total</p>
                   <p class="estimate-shipping">${formatCurrencyBRL(
-          inCashPrice
-        )}</p>
+                    inCashPrice
+                  )}</p>
                 </div>
-                ${percentDiscount > 0
-            ? `<div class="discount-percent" style="font-size: 12px; display: flex; justify-content: flex-end;">
+                ${
+                  percentDiscount > 0
+                    ? `<div class="discount-percent" style="font-size: 12px; display: flex; justify-content: flex-end;">
                         <p>(${percentDiscount}% de desconto)</p>
                       </div>`
-            : ''
-          }
+                    : ''
+                }
 
                 <div class="discount-price" style="font-size: 14px; margin-top: 10px; display: flex; justify-content: space-between;">
                     <p class="gross-total">
@@ -1150,7 +1162,6 @@ export class CheckoutCustom {
     await this.imgEmptyCart()
 
     const updateDebounce = debounce(function () {
-
       if (orderForm.marketingData) {
         this.showCustomMsgCoupon(orderForm)
       }
@@ -1200,7 +1211,7 @@ export class CheckoutCustom {
     if (
       !this.accordionPayments ||
       $('.payment-group-list-btn').find('.v-custom-payment-item-wrap').length >
-      0
+        0
     ) {
       return false
     }
@@ -1209,7 +1220,8 @@ export class CheckoutCustom {
 
     $('.payment-group-item').each(function () {
       $(this).wrap(
-        `<div class='v-custom-payment-item-wrap ${$(this).hasClass('active') ? 'active' : ''
+        `<div class='v-custom-payment-item-wrap ${
+          $(this).hasClass('active') ? 'active' : ''
         }'></div>`
       )
     })
@@ -1363,7 +1375,7 @@ export class CheckoutCustom {
                 if (itemsToRemove.length > 0) {
                   return window.vtexjs.checkout
                     .removeItems(itemsToRemove)
-                    .then(() => { })
+                    .then(() => {})
                 }
               }, 2000)
             }
@@ -1462,7 +1474,7 @@ export class CheckoutCustom {
               if (itemsToRemove.length > 0) {
                 return window.vtexjs.checkout
                   .removeItems(itemsToRemove)
-                  .then(() => { })
+                  .then(() => {})
               }
             }
           }, i * interval)
@@ -1648,6 +1660,7 @@ export class CheckoutCustom {
 
     this.fixLabels()
     this.CheckoutLimit.init()
+    this.fidelidade.init()
   }
 
   start() {
@@ -1700,7 +1713,9 @@ export class CheckoutCustom {
 
       $(document).ajaxComplete(function (event, xhr, settings) {
         _this.init()
-        const acessKeyURL = settings.url.includes(`${rootPath()}/api/checkout/pub/profiles/`)
+        const acessKeyURL = settings.url.includes(
+          `${rootPath()}/api/checkout/pub/profiles/`
+        )
         if (acessKeyURL) {
           const loginSucess = xhr.statusText === 'success'
           if (loginSucess) {
@@ -1880,7 +1895,10 @@ export class CheckoutCustom {
         if (window.location.hash === '#/cart') {
           _this.Rewards.cancelRewardsDiscount()
 
-          const checkIfRemove = localStorage.getItem('@samsung/shippingResult', 'remove')
+          const checkIfRemove = localStorage.getItem(
+            '@samsung/shippingResult',
+            'remove'
+          )
 
           if (checkIfRemove) {
             $('.srp-toggle__pickup').click()
