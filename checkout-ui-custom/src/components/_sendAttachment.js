56bef5eb28786c3ab8a1223ada3d09d36d7b6e55
Fernando Aquistapace <fernando.aquistapace@corebiz.ag>
2023-10-02 11:26:07 -0300

    chore: Installation test ok

--- checkout-ui-custom/src/components/_sendAttachment.js
+++ checkout-ui-custom/src/components/_sendAttachment.js
@@ -9,6 +9,35 @@ export default class SendAttachment {
   // Método principal para enviar texto para o campo openTextField
   sendOpenTextField() {
     const { items, customData } = window.vtexjs.checkout.orderForm
+    const obsToOpenTextField = []
+
+    this.processTransportInfo(customData, items, obsToOpenTextField)
+    this.processInstallationInfo(items, obsToOpenTextField)
+    this.processInStoreInfo(obsToOpenTextField)
+
+    const finalText = this.createFinalText(obsToOpenTextField)
+    this.sendFinalTextToOpenTextField(finalText)
+
+    window.vtexjs.checkout.getOrderForm()
+  }
+
+  // Cria a string final a ser enviada para o campo openTextField
+  createFinalText(obsToOpenTextField) {
+    return obsToOpenTextField.length
+      ? obsToOpenTextField.map(item => JSON.stringify(item)).join(', ')
+      : null
+  }
+
+  // Envia a string final para o campo openTextField e armazena no localStorage
+  sendFinalTextToOpenTextField(finalText) {
+    window.vtexjs.checkout.sendAttachment('openTextField', { value: finalText })
+    localStorage.setItem('tradeInCustom', finalText)
+  }
+
+  // ====================================================== //
+  // ======================= TRADEIN ====================== //
+  // ====================================================== //
+  processTransportInfo(customData, items, obsToOpenTextField) {
     const transportCustomData = customData?.customApps?.find(
       item => item.id === 'domain'
     )
@@ -16,22 +45,6 @@ export default class SendAttachment {
       ? JSON.parse(transportCustomData.fields.trade_in_option_selected)
       : []
 
-    const obsToOpenTextField = []
-
-    this.processTransportInfo(transport, items, obsToOpenTextField)
-    this.processInstallationInfo(items, obsToOpenTextField)
-    this.processInStoreInfo(obsToOpenTextField)
-
-    const finalText = this.createFinalText(obsToOpenTextField)
-
-    this.sendFinalTextToOpenTextField(finalText)
-
-    // !Atention
-    window.vtexjs.checkout.getOrderForm()
-  }
-
-  // Processa informações relacionadas ao transporte
-  processTransportInfo(transport, items, obsToOpenTextField) {
     transport.forEach(itemLinkTradeIn => {
       const itemLinkTradeInValid = items.filter(
         item => itemLinkTradeIn.mainProductId === item.productId
@@ -60,7 +73,9 @@ export default class SendAttachment {
     })
   }
 
-  // Processa informações relacionadas à instalação
+  // ====================================================== //
+  // ===================== INSTALATION ==================== //
+  // ====================================================== //
   processInstallationInfo(items, obsToOpenTextField) {
     const productsInstallation = items.filter(item => this.isInstallation(item))
 
@@ -95,40 +110,29 @@ export default class SendAttachment {
     })
   }
 
-  // Processa informações relacionadas à loja física ("instore")
+  // ====================================================== //
+  // ======================= INSTORE ====================== //
+  // ====================================================== //
   processInStoreInfo(obsToOpenTextField) {
-    const of = window.vtexjs.checkout.orderForm
+    const orderForm = window.vtexjs.checkout.orderForm
 
-    if (of) {
+    if (orderForm) {
       const ofMarketingData =
-        !!of.marketingData === false
+        !!orderForm.marketingData === false
           ? false
-          : !!of.marketingData.marketingTags !== false
+          : !!orderForm.marketingData.marketingTags !== false
 
       if (ofMarketingData) {
-        const isInStore = of.marketingData.marketingTags.some(
+        const isInStore = orderForm.marketingData.marketingTags.some(
           tag => tag.toLowerCase() === 'instore'
         )
 
         if (isInStore) {
-          if (of.openTextField.value.indexOf('instore') < 0) {
-            obsToOpenTextField.push({ instore: of.openTextField.value })
+          if (orderForm.openTextField.value.indexOf('instore') < 0) {
+            obsToOpenTextField.push({ instore: orderForm.openTextField.value })
           }
         }
       }
     }
-  }
-
-  // Cria a string final a ser enviada para o campo openTextField
-  createFinalText(obsToOpenTextField) {
-    return obsToOpenTextField.length
-      ? obsToOpenTextField.map(item => JSON.stringify(item)).join(', ')
-      : null
-  }
-
-  // Envia a string final para o campo openTextField e armazena no localStorage
-  sendFinalTextToOpenTextField(finalText) {
-    window.vtexjs.checkout.sendAttachment('openTextField', { value: finalText })
-    localStorage.setItem('tradeInCustom', finalText)
   }
 }
