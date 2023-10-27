f5732248cf7781bf3d636d777296f1dc3f4634b8
Daniel Miranda <daniel.sousa@trinto.com.br>
2023-10-27 17:23:46 -0300

    group discount by title

--- checkout-ui-custom/src/components/_discounts.js
+++ checkout-ui-custom/src/components/_discounts.js
@@ -149,8 +149,16 @@ export default class Discounts {
     $totalizers.each(function(_, element) {
       const $totalizer = $(element)
       const $tr = $totalizer.find('.Discounts')
+      const $trDiscountsWithTitle = discountsWithTitle.reduce((acc, next) => {
+        const exits = acc.find(item => item.title.toLowerCase().trim() === next.title.toLowerCase().trim());
+        if (!exits) {
+          return [...acc, next]
+        }
 
-      const $trDiscountsWithTitle = discountsWithTitle.map((item) => {
+        exits.value += next.value
+
+        return acc
+      },[]).map((item) => {
         return _this._discountTemplate({ 
           identifier: item.identifier,
           title: item.title,
