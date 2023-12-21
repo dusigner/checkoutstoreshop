3609e61f60f4e541b4ea015e6eeeaa717bf3c409
Lucas Poio D Oliveira Bressan <lucas.poio@samsung.com>
2023-12-21 14:19:23 +0000

    Revert "change rewards discount calc (pull request #386)"

--- checkout-ui-custom/src/components/rewards/_layout.js
+++ checkout-ui-custom/src/components/rewards/_layout.js
@@ -110,7 +110,7 @@ function createLayoutMessageObs(saguid) {
 
 function createLayoutRewardsTotalDiscount({ discount }) {
   const _component = `
-  <tr class="rewards-total-discount" style="grid-area: rewards;">
+  <tr class="rewards-total-discount">
     <td class="info">Rewards</td>
     <td class="space"></td>
     <td class="monetary" style="color: #000">- ${discount.toLocaleString(
