c6e541d6b11e87c351bbc7ea1ef7ba234bd27bb9
lucas.poio <lucas.poio@samsung.com>
2024-02-01 17:44:22 -0300

    Fix (ECOMS-10904): Adjusts change function getClientProfileData in profile.js for _utils.js function

--- checkout-ui-custom/src/components/_utils.js
+++ checkout-ui-custom/src/components/_utils.js
@@ -31,3 +31,20 @@ export const formatNegativeValue = _value => {
 
 export const formatNumberBRL = _value =>
   new Intl.NumberFormat('pt-BR').format(_value)
+
+export function rootPath() {
+  return window.__RUNTIME__.rootPath ? window.__RUNTIME__.rootPath : ''
+}
+
+export function getClientProfileData() {
+  return $.ajax({
+    url: `${rootPath()}/_v/private/aem-masterdata/v1/get/clients/custom`,
+    headers: {
+      Accept: 'application/vnd.vtex.ds.v10+json',
+      'Content-Type': 'application/json',
+    },
+    cache: false,
+    crossDomain: true,
+    type: 'GET',
+  })
+}
