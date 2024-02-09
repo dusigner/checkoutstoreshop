2882fde8b4e15e41f13aa8c2012e7882090f01fc
lucas.poio <lucas.poio@samsung.com>
2024-02-09 16:57:37 -0300

    Fix Problems with sales channel differents in samsungbrshopfidelidade

--- checkout-ui-custom/src/components/_shipping.js
+++ checkout-ui-custom/src/components/_shipping.js
@@ -437,7 +437,7 @@ export default class CustomShippingData {
     const salesChannelValidate = vtexjs.checkout.orderForm.salesChannel;
     const hasMarketingTagEndless = vtexjs.checkout.orderForm.marketingData?.marketingTags?.find(item => item === "endlessaisle")
     const salesChannelShop = ["1","5","11","12","60"].includes(salesChannelValidate)
-    const salesChannelFidelidade = ["1","70","72","78"].includes(salesChannelValidate)
+    const salesChannelFidelidade = ["1","5","11","70","72","78"].includes(salesChannelValidate)
 
     if (salesChannelValidate === "56" && hasInvalidPrice) {
       vtexjs.checkout.removeAllItems();
