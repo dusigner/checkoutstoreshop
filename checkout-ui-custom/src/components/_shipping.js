d4233127f8b887a305070271f5e34479b3d87a10
lucas.poio <lucas.poio@samsung.com>
2024-02-26 15:33:42 -0300

    adjusts in sc checkout block fidelity

--- checkout-ui-custom/src/components/_shipping.js
+++ checkout-ui-custom/src/components/_shipping.js
@@ -437,12 +437,18 @@ export default class CustomShippingData {
     const salesChannelValidate = vtexjs.checkout.orderForm.salesChannel;
     const hasMarketingTagEndless = vtexjs.checkout.orderForm.marketingData?.marketingTags?.find(item => item === "endlessaisle")
     const salesChannelShop = ["1","5","11","12","60"].includes(salesChannelValidate)
+    const salesChannelFidelidade = ["1","5","11","70","72","78","48","50","2","44","54","3","18","27","30","31","43","59","61","69"].includes(salesChannelValidate)
 
     if (salesChannelValidate === "56" && hasInvalidPrice) {
       vtexjs.checkout.removeAllItems();
     }
     if(window.vtex.accountName === "samsungbrshop" && !hasMarketingTagEndless){
       if (!salesChannelShop) {
+        vtexjs.checkout.removeAllItems();
+      }
+    }
+    if(window.vtex.accountName === "samsungbrshopfidelidade"){
+      if (!salesChannelFidelidade) {
         vtexjs.checkout.removeAllItems();
       }
     }
