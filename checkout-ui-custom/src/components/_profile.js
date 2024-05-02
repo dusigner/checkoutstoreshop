694abde9d58e63827ee0eaba2beab57d7b348553
RobertoGomesDantas <r.dgomes@samsung.com>
2024-05-02 10:36:25 -0300

    fix: whatsapp

--- checkout-ui-custom/src/components/_profile.js
+++ checkout-ui-custom/src/components/_profile.js
@@ -125,13 +125,19 @@ export default class CustomProfileData {
   async persistClientProfileData() {
     const _this = this
     const cookieSession = await _this.getSessionCookie()
-    const account = cookieSession.namespaces.account.accountName.value
+    const account = __RUNTIME__.account;
+    let authToken;
+    if ('VtexIdclientAutCookie_' in cookieSession.namespaces.cookie) {
+      authToken = cookieSession.namespaces.cookie["VtexIdclientAutCookie_" + account].value
+    } else {
+      authToken = cookieSession.namespaces.cookie["VtexIdclientAutCookie"].value;
+    }
     try {
       const { email } = window.vtexjs.checkout.orderForm.clientProfileData
       const whatsAppResponse = await fetch(`${_this.rootPath()}/_v/private/conversation/v1/frontend`, {
         method: 'POST',
         headers: {
-          'vtexAuth': cookieSession.namespaces.cookie["VtexIdclientAutCookie_" + account].value,
+          'vtexAuth': authToken,
           'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
@@ -540,12 +546,20 @@ export default class CustomProfileData {
 
     async function updateWhatsappConsent(isChecked) {
       const cookieSession = await _this.getSessionCookie();
-      const account = cookieSession.namespaces.account.accountName.value;
+      const account = __RUNTIME__.account;
+      const { orderFormId } = window.vtexjs.checkout.orderForm;
+      let authToken;
+      if ('VtexIdclientAutCookie_' in cookieSession.namespaces.cookie) {
+        authToken = cookieSession.namespaces.cookie["VtexIdclientAutCookie_" + account].value
+      } else {
+        authToken = cookieSession.namespaces.cookie["VtexIdclientAutCookie"].value;
+      }
+
       try {
         await fetch(`${_this.rootPath()}/_v/private/conversation/v1/frontend`, {
             method: 'POST',
             headers: {
-                'vtexAuth': cookieSession.namespaces.cookie["VtexIdclientAutCookie_" + account].value,
+                'vtexAuth': authToken,
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
@@ -558,6 +572,7 @@ export default class CustomProfileData {
                 }
             })
         });
+        
       } catch (err) {
           console.error(`Erro ao fazer consentimento de usuário: ${err}`);
       }
