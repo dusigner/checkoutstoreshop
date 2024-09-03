72dd14f899b0f5b5d6f2b26b80071b9eae483655
deennys <dennys.a@samsung.com>
2024-09-03 16:07:13 -0300

    fix/vale-presente: fix bug provider

--- checkout-ui-custom/src/components/_summaryGiftCard.js
+++ checkout-ui-custom/src/components/_summaryGiftCard.js
@@ -7,12 +7,12 @@ import { formatCurrencyBRL } from "./_utils"
 export default class SummaryGiftCard {
 
   floatCurrency(value) {
-    if(value){
+    if (value) {
       const mainValue = value.toString()
       const reais = mainValue.substr(0, (mainValue.length - 2))
       const centavos = mainValue.substr(-2)
       const formatedValue = parseFloat(`${reais}.${centavos}`)
-      
+
       return formatedValue
     }
   }
@@ -29,7 +29,7 @@ export default class SummaryGiftCard {
       giftCards.map(gift => {
 
         if (gift.value) {
-          
+
           count++
           const { value } = gift
           vale += this.floatCurrency(value)
@@ -47,7 +47,7 @@ export default class SummaryGiftCard {
 
           }
 
-          total = parseFloat(carrinho - vale) 
+          total = parseFloat(carrinho - vale)
 
         } else {
 
@@ -57,7 +57,7 @@ export default class SummaryGiftCard {
           }
         }
       })
-      
+
       if (total > 0) {
         $('.estimate-shipping').text(formatCurrencyBRL(total, false))
       }
@@ -68,23 +68,23 @@ export default class SummaryGiftCard {
     }
   }
 
-  removeGlobalName(){
-    const checkTerm = setInterval(()=>{
-      if(document.querySelectorAll('.gift-card-friendly-name')){
+  removeGlobalName() {
+    const checkTerm = setInterval(() => {
+      if (document.querySelectorAll('.gift-card-friendly-name')) {
         document.querySelectorAll('.gift-card-friendly-name').forEach(el => {
-          if(el.innerText.indexOf('global') >= 0)
-          el.innerText = el.innerText.split('.')[1]
+          if (el.innerText.indexOf('global') >= 0)
+            el.innerText = el.innerText.split('.')[1]
         })
         clearInterval(checkTerm)
       }
-      if(document.querySelectorAll('.payment-discoints-table span[data-bind="text: friendlyName"]')){
+      if (document.querySelectorAll('.payment-discoints-table span[data-bind="text: friendlyName"]')) {
         document.querySelectorAll('.payment-discoints-table span[data-bind="text: friendlyName"]').forEach(el => {
-          if(el.innerText.indexOf('global') >= 0)
-          el.innerText = el.innerText.split('.')[1]
+          if (el.innerText.indexOf('global') >= 0)
+            el.innerText = el.innerText.split('.')[1]
         })
         clearInterval(checkTerm)
       }
-    },50)
+    }, 50)
   }
 
   addMessage() {
@@ -101,22 +101,77 @@ export default class SummaryGiftCard {
     $('.payment-body').after(el)
   }
 
-
-  checkGiftBlock(){
-    const checkElement = setInterval(()=>{
-      if(document.querySelector('.payment-discounts-alert-wrap')){
+  checkGiftBlock() {
+    const checkElement = setInterval(() => {
+      if (document.querySelector('.payment-discounts-alert-wrap')) {
         clearInterval(checkElement)
 
-        document.querySelector('.payment-discounts-alert-wrap').addEventListener('click', (e)=>{
+        document.querySelector('.payment-discounts-alert-wrap').addEventListener('click', (e) => {
           e.preventDefault()
           vtexid.start()
         })
       }
-    },300)
+    }, 300)
+  }
+
+  buttonAddGift(orderForm) {
+    try {
+      const checkElement = setInterval(() => {
+        const btn = document.querySelector('#btn-add-gift-card')
+        if (btn) {
+          clearInterval(checkElement)
+
+          const newBtn = btn.cloneNode(true);
+          btn.parentNode.replaceChild(newBtn, btn);
+
+          newBtn.addEventListener('click', (e) => {
+            e.preventDefault()
+            e.target.disabled = true
+
+
+            const giftCard = document.querySelector('input#payment-discounts-code').value
+            console.log('giftCard: ', giftCard);
+
+            const url = `/api/checkout/pub/orderForm/${orderForm.orderFormId}/attachments/paymentData`;
+
+            const headers = {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            };
+
+            const data = {
+              "giftCards": [
+                {
+                  "redemptionCode": giftCard,
+                  "inUse": true,
+                  "isSpecialCard": false,
+                  "provider": "VtexGiftCard"
+                }
+              ]
+            };
+
+            fetch(url, {
+              method: 'POST',
+              headers: headers,
+              body: JSON.stringify(data)
+            }).then(() => {
+              e.target.disabled = false
+              vtexjs.checkout.getOrderForm()
+            }).catch(error => {
+              e.target.disabled = false
+              console.error('Error:', error)
+            });
+          })
+        }
+      }, 300)
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   init(orderForm) {
     this.giftCard(orderForm)
+    this.buttonAddGift(orderForm)
     this.removeGlobalName()
     this.checkGiftBlock()
     this.addMessage();
