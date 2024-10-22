ee0fdf702d885acca15e87a6fcdfb5254b709a73
Agnaldo Filho <agnaldo.filho@maeztra.com>
2024-10-22 13:08:35 -0300

    Revert "feat(showAutomationPayment): create function to show payment and mdw to verify automation user (eengs-3286)"
    This reverts commit e785a9011604c73d76eb2d85b3554782694b4f4d.

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,14 +1,9 @@
 import { IOClients } from '@vtex/api'
 
 import { SaveVB } from './server-settings'
-import SessionClient from './sessionClient'
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
   public get server() {
     return this.getOrSet('server', SaveVB)
-  }
-
-  public get getSession() {
-    return this.getOrSet('SessionClient', SessionClient)
   }
 }
