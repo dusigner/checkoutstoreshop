a959dcdb0128afd2c622c0eed3ff54588832c26e
Yuri Silva <yuri.silva@partner.sdslasupport.com>
2025-04-09 15:26:41 +0000

    fix: adapter ssh -> http

--- ci/create-release.sh
+++ ci/create-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git remote set-url origin git@bitbucket.org:${BITBUCKET_REPO_FULL_NAME}.git
-git config user.email cepjbcrwul6f8z6q9ylip1ve48taho@bots.bitbucket.org
+git remote set-url origin https://x-token-auth:${ACCESS_TOKEN_BITBUCKET}@bitbucket.org/sdsla/samsungbr.store-checkout.git
+git config user.email ${EMAIL_BOT_BITBUCKET}
 VERSION=$(git log --pretty='format:%h' -1)
 BRANCH_RELEASE="release/$VERSION"
 git checkout -b $BRANCH_RELEASE
