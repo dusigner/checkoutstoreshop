7d9ea9665d0a1971bccf342b69d569315bbb786b
Lucas Poio D Oliveira Bressan <lucas.poio@samsung.com>
2025-04-14 14:47:13 +0000

    Merged in fix/ecoms18675-adjusts-redirect-samsung-account-checkout (pull request #515)
    Fix (ECOMS-18675): adjusts redirect samsung account for checkout profile
    
    * Fix (ECOMS-18675): adjusts redirect samsung account for checkout profile
    
    
    Approved-by: Rafael Borges
    Approved-by: Pablo torres silva

--- checkout-ui-custom/src/components/_pre-email.js
+++ checkout-ui-custom/src/components/_pre-email.js
@@ -75,10 +75,8 @@ export default class CustomPreEmail {
   }
 
   bindEvents() {
-    const _this = this
-
     $(document).on('click', '#btn-samsung-account', function () {
-      const returnUrl = encodeURIComponent(window?.location?.pathname);
+      const returnUrl = encodeURIComponent('/checkout/#/profile');
       const currentUrl = window?.location?.href;
       let newUrl;
       const oAuthRedirect = `/login?oAuthRedirect=Samsung&returnUrl=${returnUrl}`
