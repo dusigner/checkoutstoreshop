60c6aeeb75b0143b134df42ad5b9f469f42c1c71
Lucas Poio D Oliveira Bressan <lucas.poio@samsung.com>
2025-04-28 17:07:40 +0000

    Merged in release/prod (pull request #525)
    Release/prod
    
    * Merged in feat/ECOMS-18430 (pull request #517)
    
    ISSUE 13/20/22 - CARRINHO - [GLOBAL] Revisão de preços
    
    * Feat(ECOMS-18430): TEST 28/IMP - TÍTULO E HIGHLIGHT DE PAGAMENTO CART
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into feat/ECOMS-18430
    
    * Feat(ECOMS-18430): add blue color to .new-discount-total
    
    
    Approved-by: Lucas Poio D Oliveira Bressan
    
    * Merged in feat/ecoms-18593 (pull request #516)
    
    Feat/ecoms 18593
    
    * fix in rewards when put customData
    
    
    Approved-by: David Barbosa Torres
    
    * Release "0.3.169" - master
    
    * Release validations
    
    * release rollback
    
    * Release 0.3.180 - master
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into release/prod
    
    * Merged in ecoms18701 (pull request #519)
    
    Ecoms18701
    
    * order payment
    
    * Merge branch 'release/prod' of bitbucket.org:sdsla/samsungbr.store-checkout into ecoms18701
    
    * change order payment
    
    
    Approved-by: Lucas Poio D Oliveira Bressan
    
    * Merged in fix/adjusts-copys-informations-payments (pull request #518)
    
    Fix/adjusts copys informations payments
    
    * Fix Adjusts in copys informations payments nubank and mercadopago
    
    * Fix adjusts in layout and text
    
    * Fix adjusts in functions copys payments
    
    * Merge branches 'fix/adjusts-copys-informations-payments' and 'release/prod' of https://bitbucket.org/sdsla/samsungbr.store-checkout into fix/adjusts-copys-informations-payments
    
    
    Approved-by: Rafael Borges
    
    * Merged in feat/ECOMS-18418 (pull request #506)
    
    Feat(ECOMS-18418): TEST 16/IMP - CHECKBOX IN PAYMENT METHOD
    
    * Feat(ECOMS-18418): TEST 16/IMP - CHECKBOX IN PAYMENT METHOD
    
    * Feat(ECOMS-18418): TEST 16/IMP - CHECKBOX IN PAYMENT METHOD
    
    * Feat(ECOMS-18418): add conditional text to pix payment
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into feat/ECOMS-18418
    
    * Feat(ECOMS-18418): fix style on mobile
    
    * Feat(ECOMS-18418): fix itaucard icon position
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into feat/ECOMS-18418
    
    
    Approved-by: Dennys Alvarenga do Nascimento
    
    * Release "0.3.182" Adjusts and version
    
    * Merged in fix/adjusts-login-status-adobe (pull request #524)
    
    Fix adjusts in login Status digitalData
    
    * Fix (ECOMS-18814): Adjusts in loginStatus(true) set in digitalData samsung account users
    
    * Merged in release/prod (pull request #522)
    
    Release/prod
    
    * Merged in feat/ECOMS-18430 (pull request #517)
    
    ISSUE 13/20/22 - CARRINHO - [GLOBAL] Revisão de preços
    
    * Feat(ECOMS-18430): TEST 28/IMP - TÍTULO E HIGHLIGHT DE PAGAMENTO CART
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into feat/ECOMS-18430
    
    * Feat(ECOMS-18430): add blue color to .new-discount-total
    
    
    Approved-by: Lucas Poio D Oliveira Bressan
    
    * Merged in feat/ecoms-18593 (pull request #516)
    
    Feat/ecoms 18593
    
    * fix in rewards when put customData
    
    
    Approved-by: David Barbosa Torres
    
    * Release "0.3.169" - master
    
    * Release validations
    
    * release rollback
    
    * Release 0.3.180 - master
    
    * Merge branch 'master' of https://bitbucket.org/sdsla/samsungbr.store-checkout into release/prod
    
    * Merged in ecoms18701 (pull request #51…
    * iteMerge branches 'release/prod' and 'master' of bitbucket.org:sdsla/samsungbr.store-checkout into release/prod
    
    * Release 0.3.190
    
    
    Approved-by: Dennys Alvarenga do Nascimento
    Approved-by: Gustavo Dinis Viana
    Approved-by: Erica Lucinhuki
    Approved-by: Pablo torres silva

--- checkout-ui-custom/src/components/_adobeLaunchPixel.js
+++ checkout-ui-custom/src/components/_adobeLaunchPixel.js
@@ -926,42 +926,36 @@ class AdobeLaunchPixel {
   }
   _trackLogin() {
     const { orderForm } = window.vtexjs.checkout
-    const customerLogged = orderForm.clientProfileData
+    const customerLogged = orderForm.loggedIn
     const saGuid = localStorage.getItem('saGuid')
     const loginStatus = window.digitalData.user.loginStatus
-    try {
       if (
         window._satellite !== undefined &&
         window._satellite !== null &&
         'track' in window._satellite
       ) {
-        if (
-          (window.location.hash === '#/shipping' ||
-            window.location.hash === '#/payment' ||
-            window.location.hash === '#/profile') &&
-          !loginStatus &&
-          saGuid
-        ) {
-          window.digitalData.user.loginStatus = true
-          window._satellite.track('samsung_account_login')
+        if (window.location.hash === '#/shipping' ||
+          window.location.hash === '#/payment' ||
+          window.location.hash === '#/profile') {
+          if (!loginStatus && saGuid) {
+            window.digitalData.user.loginStatus = true
+            window._satellite.track('samsung_account_login')
+          } else {
+            window.digitalData.user.loginStatus = true
+          }
         }
-        if (
-          window.location.hash === '#/cart' ||
-          window.location.hash === '#/email'
-        ) {
-          if (customerLogged !== null && saGuid) {
+        if (window.location.hash === '#/cart' ||
+          window.location.hash === '#/email') {
+          if (customerLogged !== null) {
             window.digitalData.user.loginStatus = true
           } else {
             if (saGuid) {
-              localStorage.setItem('saGuid', '')
+              localStorage.removeItem('saGuid')
             }
             window.digitalData.user.loginStatus = false
           }
         }
       }
-    } catch (e) {
-      console.error('[DTM]: Error window._satellite.track')
-    }
   }
 
   _pageTrackCart() {
@@ -970,10 +964,13 @@ class AdobeLaunchPixel {
         window._satellite !== undefined &&
         window._satellite !== null &&
         'track' in window._satellite
+      
       ) {
         if (window.digitalData.product) {
           setTimeout(() => {
+           
             window._satellite.track('page_view')
+         
           }, 1000)
         }
       }
