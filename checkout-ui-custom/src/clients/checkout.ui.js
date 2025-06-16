ba3e30249790b0e5d3cf7c3a1de1d4c425b973c6
deennys <dennys.a@samsung.com>
2025-06-16 17:16:01 -0300

    release 0.3.218

--- checkout-ui-custom/src/clients/checkout.ui.js
+++ checkout-ui-custom/src/clients/checkout.ui.js
@@ -1173,6 +1173,12 @@ couponInfo(response) {
           g => g.provider === 'SSG_REWARDS'
         )
 
+        const continueButton = $('.summary-template-holder .cart-links-bottom')
+
+        if (continueButton.length > 0) {
+          continueButton?.[1]?.setAttribute('style', 'display: none !important')
+        }
+
         let discount = 0
 
         if (
