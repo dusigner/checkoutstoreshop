34f1302311e9e6e57c6e9225c9e75e86ac8f6be7
Deyvison Rezende da Silva <deyvison.silva@partner.sdslasupport.com>
2025-08-19 13:30:15 +0000

    Merged in bugfix/PE-3571 (pull request #603)
    Bugfix/PE-3571
    
    * fix(tags): adjust rules to get tags from MD (PE-3571)
    
    * fix(tags): adjust rules to get tags from MD (PE-3571)
    
    * Merge branch 'master' of bitbucket.org:sdsla/samsungbr.store-checkout into bugfix/PE-3571
    
    
    Approved-by: Gustavo Dinis Viana
    Approved-by: Erica Lucinhuki
    Approved-by: Andrey Faiotto Clemente
    Approved-by: Marcell Paulino Pereira
    Approved-by: Pablo torres silva

--- checkout-ui-custom/src/onepage/clients/checkout.ui.js
+++ checkout-ui-custom/src/onepage/clients/checkout.ui.js
@@ -408,8 +408,8 @@ couponInfo(response) {
           const tagList = window.samsungProductTags || [];
           const matchedTag = tagList.find(tag =>
             tag.productIdTag === String(productId) &&
-            tag.productTag === "Instalado pela Samsung" &&
-            tag.activeTag === true
+            tag.activeTag === true &&
+            tag.tagCheckoutVisible === true
           );
  
           const installedHtml = matchedTag
